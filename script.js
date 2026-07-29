6fddd3d10e6b6a86006dc2dbd00721ba3d32bf09
Oluwadarasimi Adeniji <adenijidarasimi894@gmail.com>
2026-07-29 00:55:34 -0700

    new

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const heading = document.createElement('h1');
+    heading.textContent = 'Document loaded';
+    document.body.appendChild(heading);
+});
