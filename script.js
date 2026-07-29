86889c7a5c2b66f83ca8a6a1aee16030fa5993c4
Oluwadarasimi Adeniji <adenijidarasimi894@gmail.com>
2026-07-29 01:37:37 -0700

    final final push

--- script.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const heading = document.createElement('h1');
-    heading.textContent = 'Document loaded';
-    document.body.appendChild(heading);
-});
